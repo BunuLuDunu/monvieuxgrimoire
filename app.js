bc09c757dfaf82bbe26eb8701e7a2cc58747e90c
BunuLuDunu <laurine.sassano@gmail.com>
2023-08-19 09:40:22 +0200

    Added Mongoose

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+// Ajout d'Express
 const express = require('express');
-
 const app = express();
+app.use(express.json());
 
 
+// Ajout de Mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://laurinesassano:monvieuxgrimoire@cluster0.t5wh6dv.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 module.exports = app;
