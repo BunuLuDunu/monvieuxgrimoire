388fc2e8fd75d3ec103ddcaae83a0a9a2ba54cbb
BunuLuDunu <laurine.sassano@gmail.com>
2023-08-21 10:29:19 +0200

    Added seperate routing file

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// Importation du router
+const booksRoutes = require('./routes/books.js');
 
-// Ajout de Mongoose
+// Ajout de Mongoose et connection à MongoDB
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://laurinesassano:monvieuxgrimoire@cluster0.t5wh6dv.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+// Ajout du middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/books', booksRoutes);
 
 module.exports = app;
