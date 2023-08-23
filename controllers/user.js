da04791042db7efc124200f601f9fc38e64a4aaa
BunuLuDunu <laurine.sassano@gmail.com>
2023-08-23 11:00:32 +0200

    Imported bcrypt and created signup function

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,22 @@
+// Importation du package bcrypt
+const bcrypt = require('bcrypt');
+
+// Importation du model user
+const User = require('../models/User');
+
 // Fonction pour l'enregistrement de nouveaux utilisateurs
 exports.signup = (req, res, next) => {
-
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user =  new User({
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
 // Fonction pour connecter les utilisateurs existants
