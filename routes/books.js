c4a280b51e9e80ca250b7f7f90268ff44c5e8837
BunuLuDunu <laurine.sassano@gmail.com>
2023-08-24 10:56:51 +0200

    Fixed error with user connection

--- routes/books.js
+++ routes/books.js
@@ -17,14 +17,15 @@ const booksCtrl = require('../controllers/books');
 
 // Route pour créer un nouveau livre
 router.post('/', auth, booksCtrl.createBook);
+// Route pour récupérer tous les livres
+router.get('/', booksCtrl.getAllBooks);
 // Route pour la modification d'un livre
 router.put('/:id', auth, booksCtrl.modifyBook);
 // Route pour supprimer un livre
 router.delete('/:id', auth, booksCtrl.deleteBook);
 // Route pour récupérer un livre unique
-router.get('/:id', auth, booksCtrl.getOneBook);
-// Route pour récupérer tous les livres
-router.get('/', auth, booksCtrl.getAllBooks);
+router.get('/:id', booksCtrl.getOneBook);
+
 
 
 // Exportation du router
