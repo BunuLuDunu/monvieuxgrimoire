378a6011567e8d9c74c9fcfbe12a1b9f1ba6b12f
BunuLuDunu <laurine.sassano@gmail.com>
2023-08-25 11:01:16 +0200

    Multer middleware config

---
+++ middleware/multer-config.js
@@ -0,0 +1,24 @@
+// Importation de multer
+const multer = require('multer');
+
+// Dictionnaire
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+};
+
+// Création d'un objet de configuration
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    },
+    filename: (req, file, callback) => {
+        // Elimination des espaces dans les noms de fichiers originaux
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
+    }
+});
+
+module.exports = multer({ storage }).single('image');
--- models/User.js
+++ models/User.js
@@ -4,10 +4,15 @@ const mongoose = require('mongoose');
 // Importation du package de validation pour s'assurer que deux utilisateurs ne puissent pas utiliser la même adresse e-mail
 const uniqueValidator = require('mongoose-unique-validator');
 
-// Création du modèle d'utilisateur
+// Création du modèle d'utilisateur avec format d'email valide et mot de passe supérieur à 8 caractères
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true, validate: {
+        validator: (string) => {
+            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(string);
+        }, 
+        message: 'Cet email n\'est pas valide'
+    }},
+    password: { type: String, min: 8, required: true },
 });
 
 userSchema.plugin(uniqueValidator);
