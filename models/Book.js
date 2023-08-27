76df1b0118bfcc9b278bd3316984394b27ed6e24
BunuLuDunu <laurine.sassano@gmail.com>
2023-08-27 09:25:50 +0200

    Added uniqueValidator to book title

--- models/Book.js
+++ models/Book.js
@@ -1,10 +1,13 @@
 // Importation de mongoose
 const mongoose = require('mongoose');
 
+// Importation du validateur unique pour les titres de livres
+const uniqueValidator = require('mongoose-unique-validator');
+
 // Création du modèle de livre
 const bookSchema = mongoose.Schema({
     userId: { type: String, required: true },
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     author: { type: String, required: true },
     imageUrl: { type: String, required: true },
     year: { type: Number, required: true },
@@ -17,5 +20,7 @@ const bookSchema = mongoose.Schema({
     ], 
     averageRating: { type: Number, default: 0 },
 });
+
+bookSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Book', bookSchema);
